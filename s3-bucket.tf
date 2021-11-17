4742c2a266d70fa93b0076110e3109bde238d0c1
andreyzhm <andrey.zhmaylik@gmail.com>
2021-11-17 12:10:30 +0000

    New bucket has been added

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,12 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/az-traininig/s3-bucket/aws"
+  version = "2.2.0"
 
   bucket = "my-s3-bucket"
   acl    = "private"
-  bucket_prefix = ${var.prefix}
+  bucket_prefix = "${var.prefix}"
 
   versioning = {
     enabled = true
   }
-
 }
